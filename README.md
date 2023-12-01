840bc502cb33a989229da851d7fd0d0676d738ea
小何同学 <HeDianGeng0601@outlook.com>
2023-12-01 14:27:27 +0800

    feat: 替换标识xiaohe0601

--- README.md
+++ README.md
@@ -8,20 +8,14 @@
 
 _description_
 
+> Forked from [antfu/starter-ts](https://github.com/antfu/starter-ts)
+
 > **Note**:
-> Replace `pkg-placeholder`, `_description_` and `antfu` globally to use this template.
-
-## Sponsors
-
-<p align="center">
-  <a href="https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg">
-    <img src='https://cdn.jsdelivr.net/gh/antfu/static/sponsors.svg'/>
-  </a>
-</p>
+> Replace `pkg-placeholder`, `_description_` and `xiaohe0601` globally to use this template.
 
 ## License
 
-[MIT](./LICENSE) License © 2023-PRESENT [Anthony Fu](https://github.com/antfu)
+[MIT](./LICENSE) License © 2023-PRESENT [小何同学](https://github.com/xiaohe0601)
 
 
 <!-- Badges -->
@@ -32,7 +26,7 @@ _description_
 [npm-downloads-href]: https://npmjs.com/package/pkg-placeholder
 [bundle-src]: https://img.shields.io/bundlephobia/minzip/pkg-placeholder?style=flat&colorA=080f12&colorB=1fa669&label=minzip
 [bundle-href]: https://bundlephobia.com/result?p=pkg-placeholder
-[license-src]: https://img.shields.io/github/license/antfu/pkg-placeholder.svg?style=flat&colorA=080f12&colorB=1fa669
-[license-href]: https://github.com/antfu/pkg-placeholder/blob/main/LICENSE
+[license-src]: https://img.shields.io/github/license/xiaohe0601/pkg-placeholder.svg?style=flat&colorA=080f12&colorB=1fa669
+[license-href]: https://github.com/xiaohe0601/pkg-placeholder/blob/main/LICENSE
 [jsdocs-src]: https://img.shields.io/badge/jsdocs-reference-080f12?style=flat&colorA=080f12&colorB=1fa669
 [jsdocs-href]: https://www.jsdocs.io/package/pkg-placeholder
--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,67 @@
-// @ts-check
-import antfu from '@antfu/eslint-config'
+import defineConfig from "@antfu/eslint-config";
 
-export default antfu(
-  {
-    ignores: [
-      // eslint ignore globs here
-    ],
+// 配置参考
+// https://github.com/antfu/eslint-config
+// https://github.com/antfu/eslint-flat-config-viewer
+export default defineConfig({
+  stylistic: {
+    indent: 2,
+    quotes: "double"
   },
-  {
-    rules: {
-      // overrides
-    },
-  },
-)
+  ignores: []
+}, {
+  rules: {
+    "curly": ["error", "all"],
+    "no-console": ["warn", {
+      allow: [
+        "warn",
+        "error"
+      ]
+    }],
+    "dot-notation": ["off"],
+    "symbol-description": ["off"],
+    "no-extra-boolean-cast": ["off"],
+    "prefer-promise-reject-errors": ["off"],
+    "style/semi": ["error", "always"],
+    "style/eol-last": ["error", "never"],
+    "style/brace-style": ["error", "1tbs"],
+    "style/quote-props": ["off"],
+    "style/comma-dangle": ["error", "never"],
+    "style/arrow-parens": ["error", "always"],
+    "style/padded-blocks": ["error", {
+      blocks: "never",
+      classes: "always",
+      switches: "never"
+    }],
+    "style/operator-linebreak": ["error", "before", {
+      overrides: {
+        "=": "after"
+      }
+    }],
+    "style/no-multiple-empty-lines": ["error", {
+      max: 2,
+      maxBOF: 0,
+      maxEOF: 0
+    }],
+    "style/member-delimiter-style": ["error", {
+      singleline: {
+        delimiter: "semi",
+        requireLast: true
+      },
+      multiline: {
+        delimiter: "semi",
+        requireLast: true
+      },
+      multilineDetection: "brackets"
+    }],
+    "ts/ban-ts-comment": ["off"],
+    "ts/prefer-ts-expect-error": ["off"],
+    "import/no-self-import": ["off"],
+    "import/newline-after-import": ["error", {
+      count: 1,
+      considerComments: false
+    }],
+    "node/prefer-global/process": ["error", "always"],
+    "antfu/consistent-list-newline": ["off"]
+  }
+});
